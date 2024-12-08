752756516f6fd6b0603f0c4e329bcd00cdfd962a
Flynn752 <user-8738746@servn.ca>
2024-12-08 16:11:44 -0600

    Quartz sync: Dec 8, 2024, 4:11 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          <a href="https://ftw.servn.ca/">some text...</a> © {year}
+          <a href="https://ftw.servn.ca/">ftw.servn.ca</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
