de12eb242b6782c3fcaa971a85f56eb2ffbe2bfa
Flynn752 <user-8738746@servn.ca>
2024-12-08 17:05:08 -0600

    Quartz sync: Dec 8, 2024, 5:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "ftw.servn.ca",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "images"],
     defaultDateType: "created",
     generateSocialImages: false,
     theme: {
