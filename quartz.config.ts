3f3f320e2dbe4bd4d6a4ea1d3c5bb08dd34d88b2
Flynn752 <user-8738746@servn.ca>
2024-12-08 21:04:43 -0600

    Quartz sync: Dec 8, 2024, 9:04 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "For-The-Win",
+    pageTitle: "ForTheWin",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
